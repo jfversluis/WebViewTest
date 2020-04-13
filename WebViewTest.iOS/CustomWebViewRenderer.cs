5905a0adbaeaf22de47c141494d86c3cf8955ad2
Gerald Versluis <gerald@verslu.is>
2020-04-13 21:57:09 +0200

    Initial commit

---
+++ WebViewTest.iOS/CustomWebViewRenderer.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using WebViewTest;
+//using WebViewTest.iOS;
+//using Xamarin.Forms;
+//using Xamarin.Forms.Platform.iOS;
+
+//[assembly: ExportRenderer(typeof(CustomWebView), typeof(CustomWebViewRenderer))]
+
+//namespace WebViewTest.iOS
+//{
+//    public class CustomWebViewRenderer : WkWebViewRenderer
+//    {
+//    }
+//}
---
+++ WebViewTest/CustomWebView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WebViewTest
+{
+    public class CustomWebView : WebView
+    {
+        public CustomWebView()
+        {
+        }
+    }
+}
